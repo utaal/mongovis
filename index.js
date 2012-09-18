557e3ccc5ee1a5898bf3c28e3c3c4a31b23c88a2
Andrea Lattuada <andrea.lattuada@10gen.com>
2012-09-18 15:10:55 -0400

    consolidated render functions, added display of record characteristic number (e.g. age according to objectid)

--- index.js
+++ index.js
@@ -16,7 +16,8 @@
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var BAR_HEIGHT = 150;
+var AGE_BAR_HEIGHT = 50;
+var BAR_HEIGHT = 110;
 var SUMMARY_BAR_WIDTH = 20;
 
 var _data = null;
@@ -34,6 +35,12 @@ var bsonSizeToDiskSizeRatio = function(d) {
 }
 var nonBsonRecSizeToDiskSizeRatio = function(d) {
   return (d.recSize - d.bsonSize) / d.onDiskSize;
+}
+
+var charactRatio = function(boundaries) {
+  return function(d) {
+    return boundaries.ranged(d.charactSum / d.charactCount);
+  };
 }
 
 function handleFiles(files) {
@@ -56,25 +63,44 @@ function handleRender() {
   render(_data, barSize);
 }
 
+function computeCharactBoundaries(arr) {
+  var allCharact = arr.map(function(ex) {
+    return ex.charactSum / ex.charactCount;
+  }).filter(function(d) { return !isNaN(d) });
+  return {
+    min: Math.min.apply(null, allCharact),
+    max: Math.max.apply(null, allCharact),
+    ranged: function(val) {
+      return ((val - this.min) / (this.max - this.min));
+    }
+  }
+}
+
+
 function render(data, barSize) {
   //TODO(andrea.lattuada) refactor
   if (data.extents) {
     var summaryGraphs = d3.select("#summaryGraphs");
     appendDescDiv(summaryGraphs, "collection", data, "coll-desc");
+    var charactBoundaries = computeCharactBoundaries(
+        _.flatten(data.extents.map(function(ex) { return ex.chunks })));
     for (var i = 0; i < data.extents.length; ++i) {
       var extentData = data.extents[i];
-      renderExtent(i, extentData, barSize);
+      renderExtent(i, extentData, barSize, charactBoundaries);
       var collSummaryExtent = summaryGraphs.append("a")
         .attr("onclick", "highlightExtent(" + i + "); return true;")
-        .attr("href", "#extent" + i).append("div");
-      var extentBarWidth = extentData.onDiskSize / 100000;
-      if (extentBarWidth < SUMMARY_BAR_WIDTH / 2) {
-        extentBarWidth = SUMMARY_BAR_WIDTH / 2;
-      }
-      renderExtentSummary(collSummaryExtent, extentData, extentBarWidth);
+        .attr("href", "#extent" + i).append("div")
+        .attr("class", "extent-summary");
+      // var extentBarWidth = extentData.onDiskSize / 100000;
+      // if (extentBarWidth < SUMMARY_BAR_WIDTH / 2) {
+      //   extentBarWidth = SUMMARY_BAR_WIDTH / 2;
+      // }
+      renderExtentSummary(collSummaryExtent, extentData, SUMMARY_BAR_WIDTH,
+                          charactBoundaries);
     }
   } else if (data.chunks) {
-    renderExtent("", data, barSize);
+    var charactBoundaries = computeCharactBoundaries(data.chunks);
+    renderExtent("", data, barSize, charactBoundaries);
   }
 }
 
@@ -92,43 +118,44 @@ function appendDescDiv(toElm, title, data, clazz) {
       "(% of ext used: " + percentStr(data.recSize / data.onDiskSize) + ")<br/>" +
       "BSONs size: " + data.bsonSize + "<br/>" +
       "(% of ext used: " + percentStr(data.bsonSize / data.onDiskSize) + ")<br/>" +
-      "padding: " + (data.recSize / data.bsonSize).toFixed(4));
+      "padding: " + (data.recSize / data.bsonSize).toFixed(4) + "<br/>" +
+      "average charact. (age?): " +
+          (data.charactSum / data.charactCount).toFixed(4) + "<br/>");
 }
 
-function renderExtent(extentNum, extentData, barSize) {
+function renderExtent(extentNum, extentData, barSize, charactBoundaries) {
   var extentDiv = d3.select("#graphs").append("a")
     .attr("name", "extent" + extentNum).append("div").attr("class", "extent");
-  renderExtentSummary(extentDiv, extentData, SUMMARY_BAR_WIDTH);
+  renderExtentSummary(extentDiv.append("div"), extentData, SUMMARY_BAR_WIDTH,
+                      charactBoundaries);
   appendDescDiv(extentDiv, "extent " + extentNum, extentData, "extent-desc");
-  renderExtentGraph(extentDiv, extentData, barSize);
+  renderExtentGraph(extentDiv.append("div"), extentData, barSize,
+                    charactBoundaries);
 }
 
-function renderExtentSummary(div, extentData, barSize) {
-  var extentsummary = div.append("svg")
-    .attr("class", "extent-summary")
-    .attr("width", barSize)
-    .attr("height", BAR_HEIGHT);
-  extentsummary.append("rect")
-    .attr("class", "bsonSize")
-    .attr("width", barSize)
-    .attr("height", bsonSizeToDiskSizeRatio(extentData) * BAR_HEIGHT);
-  extentsummary.append("rect")
-    .attr("class", "recSize")
-    .attr("width", barSize)
-    .attr("y", bsonSizeToDiskSizeRatio(extentData) * BAR_HEIGHT)
-    .attr("height", nonBsonRecSizeToDiskSizeRatio(extentData) * BAR_HEIGHT);
+function renderExtentSummary(div, extentData, barSize, charactBoundaries) {
+  renderGraph(div, [extentData], barSize, charactBoundaries);
 }
 
-function renderExtentGraph(div, extentData, barSize) {
+function renderExtentGraph(div, extentData, barSize, charactBoundaries) {
+  return renderGraph(div, extentData.chunks, barSize, charactBoundaries);
+}
+
+function renderGraph(div, arr, barSize, charactBoundaries) {
   var EXTENT_PADDING = 1;
-  var chunkOnDiskSizes = extentData.chunks.map(function (d) {
-    return d.onDiskSize
+  var chunkOnDiskSizes = arr.map(function (d) {
+    return d.onDiskSize;
   });
   var maxChunkLength = d3.max(chunkOnDiskSizes);
+  var fullWidth = d3.sum(chunkOnDiskSizes) / maxChunkLength * barSize +
+                  arr.length + EXTENT_PADDING;
+  var extentchartcharact = div.append("svg")
+    .attr("class", "extent-chart extent-chart-charact")
+    .attr("width", fullWidth)
+    .attr("height", AGE_BAR_HEIGHT);
   var extentchart = div.append("svg")
-    .attr("class", "extent-chart")
-    .attr("width", d3.sum(chunkOnDiskSizes) / maxChunkLength * barSize +
-          extentData.chunks.length + EXTENT_PADDING)
+    .attr("class", "extent-chart extent-chart-usage")
+    .attr("width", fullWidth)
     .attr("height", BAR_HEIGHT);
   var x = function() {
     var nextX = 0;
@@ -138,18 +165,46 @@ function renderExtentGraph(div, extentData, barSize) {
       return curX + EXTENT_PADDING;
     };
   };
+  var charactheight = d3.scale.linear()
+    .domain([0, 1])
+    .range([0, AGE_BAR_HEIGHT]);
+  var characty = d3.scale.linear()
+    .domain([1, 0])
+    .range([0, AGE_BAR_HEIGHT]);
   var y = d3.scale.linear()
     .domain([0, 1])
     .range([0, BAR_HEIGHT]);
   var xsize = function (d, i) {
     return d.onDiskSize / maxChunkLength * barSize;
   }
-  var enter = extentchart.selectAll("rect").data(extentData.chunks).enter()
+  extentchartcharact.selectAll("rect").data(arr)
+    .enter().append("rect")
+    .attr("class", "charact")
+    .attr("x", x())
+    .attr("width", xsize)
+    .attr("y", compose(characty, charactRatio(charactBoundaries)))
+    .attr("height", compose(charactheight, charactRatio(charactBoundaries)));
+  var xfunc = x();
+  extentchartcharact.selectAll("g").data(arr)
+    .enter().append("svg:g")
+    .each(function (d, i) {
+      var xpos = xfunc(d);
+      console.log(i + ", " + xpos);
+      if (!d.charactCount) {
+        d3.select(this).append("rect")
+          .attr("class", "missing-datum")
+          .attr("x", xpos)
+          .attr("width", xsize(d))
+          .attr("height", 5)
+          .attr("y", AGE_BAR_HEIGHT - 10);
+      }
+    });
+  var enter = extentchart.selectAll("rect").data(arr).enter();
   enter.append("rect")
     .attr("class", "bsonSize")
     .attr("x", x())
     .attr("width", xsize)
-    .attr("height", compose(y, bsonSizeToDiskSizeRatio))
+    .attr("height", compose(y, bsonSizeToDiskSizeRatio));
   enter.append("rect")
     .attr("class", "recSize")
     .attr("x", x())
